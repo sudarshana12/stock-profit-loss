1ccb31228ca8053177857c6dd6643ae5ef5a7794
Sudarshana Tamuly <47175592+sudarshana12@users.noreply.github.com>
2021-10-30 15:19:00 +0530

    updated

--- app.js
+++ app.js
@@ -3,3 +3,35 @@ var stocksQuantity = document.querySelector("#stocks-quantity");
 var currentPrice = document.querySelector("#current-price");
 var submitBtn = document.querySelector("#submit-btn");
 var outputBox = document.querySelector("#output-box");
+
+function calculateProfitAndLoss(initial,quantity,current){
+    if(initial > current){
+        var loss = (initial - current)*quantity;
+        var lossPercentage = (loss / initial) * 100;
+        showOutput(`Hey the loss is ${loss} and the percent is ${lossPercentage}%`);
+
+
+    }else if(current > initial){
+        var profit = (current - initial)*quantity;
+        var profitPercentage = (profit / initial) * 100;
+        showOutput(`Hey the profit is ${profit} and the percent is ${profitPercentage}%`);
+
+    } else {
+        showOutput(`No pain no gain and no gain no pain`);
+
+    }
+}
+
+function submitHandler(){
+    var ip = Number(initialPrice.value);
+    var qty = Number(stocksQuantity.value);
+    var curr = Number(currentPrice.value);
+
+    calculateProfitAndLoss(ip, qty, curr);
+}
+
+submitBtn.addEventListener('click', submitHandler);
+
+function showOutput(msg){
+    outputBox.innerHTML = msg;
+}
