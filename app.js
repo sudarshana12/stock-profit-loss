ac15c1965544c4a5372476f88652afff765753dc
Sudarshana Tamuly <47175592+sudarshana12@users.noreply.github.com>
2021-10-30 13:43:48 +0530

    firstdraft

---
+++ app.js
@@ -0,0 +1,5 @@
+var initialPrice = document.querySelector("#initial-price");
+var stocksQuantity = document.querySelector("#stocks-quantity");
+var currentPrice = document.querySelector("#current-price");
+var submitBtn = document.querySelector("#submit-btn");
+var outputBox = document.querySelector("#output-box");
